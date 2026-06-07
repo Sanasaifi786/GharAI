17f1bd2b7085657fc18d3df2bb8c70c390d7a7f8
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-08 01:19:07 +0530

    sync and update project dependencies in node_modules

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="home">
+        <h1>Buy your dream home</h1>
+        <p>Search millions of properties</p>
+      </div>
+    </>
+  )
+}
+
+export default App
